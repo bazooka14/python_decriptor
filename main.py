c9c0f77f5f93203fc5d5315e54ab3b9d33833483
Magomedpasha Sultanov <sultanov.mps@mail.ru>
2025-01-28 16:44:35 +0300

    how it works?

---
+++ main.py
@@ -0,0 +1,22 @@
+class Natural:
+    def __set_name__(self, owner, name):
+        self.name = "__" + name
+        
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+    
+    def __set__(self, instance, value):
+        if value > 0:
+            instance.__dict__[self.name] = value
+        else:
+            raise ValueError("X must be greater than zero")
+
+class Point:
+    x = Natural()
+    y = Natural()
+    
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+    
+point_one = Point(4, 3)
